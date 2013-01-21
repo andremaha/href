21f04f3e7be15bd6f5050626e06e7adbcb0eeca0
Andrey Esaulov <esaulov.andrey@gmail.com>
2013-01-21 18:06:36 +0100

    Registration successful screen

--- src/Href/ShortyBundle/Controller/UserController.php
+++ src/Href/ShortyBundle/Controller/UserController.php
@@ -7,30 +7,6 @@ use Symfony\Component\Security\Core\SecurityContext;
 
 class UserController extends Controller
 {
-    public function loginAction()
-    {
-        $request = $this->getRequest();
-        $session = $request->getSession();
-
-        // Store login errors
-        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
-            $error = $request->attributes->get(
-                SecurityContext::AUTHENTICATION_ERROR
-            );
-        } else {
-            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
-            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
-        }
-
-        return $this->render(
-            'HrefShortyBundle:User:login.html.twig',
-            array(
-                // last username entered by the user
-                'last_username' => $session->get(SecurityContext::LAST_USERNAME),
-                'error'         => $error
-            )
-        );
-    }
 
     public function profileAction()
     {
