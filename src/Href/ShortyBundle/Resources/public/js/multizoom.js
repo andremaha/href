19c104d46a68dd2a6a9141b93dd70c54b74a45b9
Andrey Esaulov <esaulov.andrey@gmail.com>
2013-01-22 16:47:05 +0100

    Added some zoom-in functionality on the register teaser image

---
+++ src/Href/ShortyBundle/Resources/public/js/multizoom.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($){
+
+    $('#why_register').addimagezoom({ // single image zoom
+        zoomrange: [3, 10],
+        magnifiersize: [500,250],
+        magnifierpos: 'left',
+        cursorshade: true
+        //largeimage: 'hayden.jpg' //<-- No comma after last option!
+    })
+});
